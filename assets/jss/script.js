22060cbd77046f6785a22b503b15aefd94d05c8a
Ladyofgoats <will12emery@googlemail.com>
2024-05-20 09:53:49 +0000

    Added the correct score tracking javascript.

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -39,7 +39,7 @@ function runGame(gameType) {
 }
 
 /**
- * Checks the answer agaist the first element in
+ * Checks the answer against the first element in
  * the returned calculateCorrectAnswer array
  */
 function checkAnswer() {
@@ -49,10 +49,10 @@ function checkAnswer() {
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if (isCorrect) {
-        alert("Well done, that is the correct answer!");
+        alert("You are correct this time, you may continue.");
         incrementScore();
     } else {
-        alert(`You are incorrect, you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        alert(`Wrong, you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
         incrementWrongAnswer();
     }
 
@@ -79,16 +79,24 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
-    let oldscore = parseInt(getElementById("score").innerText);
-    document.getElementById("score").innertext = ++oldscore
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-    let oldscore = parseInt(getElementById("incorrect").innerText);
-    document.getElementById("incorrect").innertext = ++oldscore
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+    
 }
 
 function displayAdditionQuestion(operand1, operand2) {
